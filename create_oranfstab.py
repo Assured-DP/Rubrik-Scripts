2cde3a66c7948fd1fb3aa55ff51684864beeff64
Andrew <andrew.eva@assured-dp.com>
2019-07-08 12:25:36 -0400

    multiple source and formats
    Formatting Update and allowing for multiple oracle ips

--- create_oranfstab.py
+++ create_oranfstab.py
@@ -2,6 +2,7 @@
 import requests
 import urllib3
 import getpass
+import time
 #requests.packages.urllib3.disable_warnings()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -10,7 +11,13 @@ global billingSize
 rubrikip = raw_input("Rubrik IP: ")
 rubrikuser = raw_input("Rubrik user: ")
 rubrikpass = getpass.getpass("Rubrik Pass: ")
+localip = raw_input("Enter IP of Oracle Host (Separate Multiple with commas): ")
 
+oracleip = []
+if "," in localip:
+	oracleip = localip.split(",")
+else:
+	oracleip.append(localip)
 
 tokenUrl = "https://"+rubrikip+"/api/v1/session"
 username = rubrikuser
@@ -19,49 +26,48 @@ session = requests.post(tokenUrl, verify=False, auth=(username, password))
 session = session.json()
 token = 'Bearer ' + session['token']
 
-localip = "172.20.160.100"
+#localip = "172.20.160.100"
 
-
+voljson = {}
 
 print "#############################"
 print "oranfstab begins here"
 print "#############################"
 
-
 def buildVolumeList():
-    vmurl = "https://"+rubrikip+"/api/internal/managed_volume"
-    volsresponse = requests.get(vmurl, headers= { 'Accept': 'application/json', 'Authorization': token },verify=False, stream=True)
-    voljson = volsresponse.json()
-    count = 1
-    if voljson['total'] > 1:
-        for volume in voljson['data']:
-            if count < 10:
-		print ("server: rbk_"+volume['name'].lower()+"_00"+str(count))
-	    elif count < 100:
-		print ("server: rbk_"+volume['name'].lower()+"_0"+str(count))
-	    else:
-		print ("server: rbk_"+volume['name'].lower()+"_"+str(count))
-	    print ("local: "+localip)
-	    count = count + 1
-	    # Print Paths
-	    for path in volume['mainExport']['channels']:
-	    	print ("path: "+path['ipAddress'])
-	    mntcount = 0
-	    for mounts in volume['mainExport']['channels']:
-		mntname = str(volume['name'])+"-ch"+str(mntcount)
-		print ("export: "+mounts['mountPoint']+" mount: /mnt/rubrik/"+mntname)
-		mntcount = mntcount + 1
-	    print("nfs_version: nfsv3")
-	    print("")
+	floatipurl = "https://"+rubrikip+"/api/internal/cluster/me/floating_ip"
+	floatresponse = requests.get(floatipurl, headers= { 'Accept': 'application/json', 'Authorization': token },verify=False, stream=True)
+	floatipjson = floatresponse.json()
+	vmurl = "https://"+rubrikip+"/api/internal/managed_volume"
+	volsresponse = requests.get(vmurl, headers= { 'Accept': 'application/json', 'Authorization': token },verify=False, stream=True)
+	print vmurl
+	global voljson
+	voljson = volsresponse.json()
+	count = 1
+	if voljson['total'] > 0:
+		for node in floatipjson['data']:
+			print ("server: "+node['nodeId'])
+			for oraip in oracleip:
+				print("local: "+oraip+" path: "+node['ip'])
+			mntcount = 0
+			for volume in voljson['data']:
+				for channel in volume['mainExport']['channels']:
+					if channel['ipAddress'] == node['ip']:
+						mntname = volume['name']+"-ch"+str(mntcount)
+						mntcount = mntcount+1
+						print("export: "+channel['mountPoint']+" mount: /mnt/rubrik/"+mntname)
+			print("nfs_version: nfsv3")
+			print("")
 
 def buildTab():
-    vmurl = "https://uscust008mon001.us.assured.local/api/internal/managed_volume"
-    volsresponse = requests.get(vmurl, headers= { 'Accept': 'application/json', 'Authorization': token },verify=False, stream=True)
-    voljson = volsresponse.json()
+    #vmurl = "https://uscust008mon001.us.assured.local/api/internal/managed_volume"
+    #volsresponse = requests.get(vmurl, headers= { 'Accept': 'application/json', 'Authorization': token },verify=False, stream=True)
+    #print volsresponse.text
+    #voljson = volsresponse.json()
     nfs_path = "/mnt/rubrik/"
-    nfs_settings = " nfs rw,noatime,bg,hard,nointr,rsize=131072,wsize=131072,tcp,actimeo=0,vers=3,timeo=600,addr="
+    nfs_settings = " nfs rw,noatime,bg,hard,nointr,rsize=1048576,wsize=1048576,tcp,actimeo=0,vers=3,timeo=600,addr="
     nfs_close = " 0 0"
-    if voljson['total'] > 1:
+    if voljson['total'] > 0:
         for volume in voljson['data']:
             # Print Paths
             mountcount = 0
@@ -74,4 +80,5 @@ print("")
 print("########################")
 print("mtab information below")
 print("########################")
+time.sleep(1)
 buildTab()
