54f7f556f4f5e4c60f76019c1f87bef7c2f74c78
Andrew <andrew.eva@assured-dp.com>
2024-02-20 12:30:26 -0500

    SDK and Replication

---
+++ replication_data_export.py
@@ -0,0 +1,241 @@
+##
+##
+##
+##
+## Reads all archive tier export data and dumps it into a CSV to be analyed
+##
+##
+import getpass
+import json
+import requests
+import urllib3
+import rubrikSDK
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+
+def generateToken(target, username, password):
+    thesession = requests.Session()
+    thesession.verify = False
+    baseurl = "https://"+target+"/api/"
+    url = baseurl+"internal/session"
+    payload = {"initParams":{}}
+    code = input("Enter TOTP Code: ")
+    response = thesession.post(url=url, auth=(username, password), json=payload)
+    #print(response.json())
+    attemptId = response.json()['mfaResponse']['attemptId']
+    data = {
+        "initParams":{}, 
+        "mfaParams":{ 
+            "attemptId":attemptId,
+            "challengeSetId":"N/A",
+            "challengeId":"rubrik-totp-auth",
+            "credValue":code
+            }
+        }
+    challengeResponse = thesession.post(url=url, timeout=120, json=data, auth=(username,password))
+    #print(challengeResponse.json())
+    return challengeResponse.json()['session']['token']
+'''
+    orgId = challengeResponse.json()['session']['organizationId']
+    tagIt = "DR"+username+"DR"
+    apiTokenData = {
+        "initParams": {
+            "apiToken": {
+                "expiration": 525000,
+                "tag": tagIt[0:19]
+                },
+            "organizationId": orgId
+            }
+        }
+    token = "Bearer "+challengeResponse.json()['session']['token']
+    header = {'Authorization': token}
+    thesession.headers = header
+    tokenCreateResponse = thesession.post(url=url, json=apiTokenData)
+    print(tokenCreateResponse.json())
+    finalToken = tokenCreateResponse.json()['session']['token']
+'''
+    
+
+# Function for Opening the Rubrik Session
+def connectRubrik(dripaddress,username,password):
+    global localprimaryclusterid, clustermeresponse
+    drrbksess = requests.Session()
+    drrbksess.verify = False
+    baseurl = "https://"+dripaddress+"/api/"
+    drsessurl = baseurl + "v1/session"
+    drtoken = generateToken(dripaddress, username, password)
+    drbearer = "Bearer " + drtoken
+    drheader = {'Authorization': drbearer}
+    drrbksess.headers = drheader
+    testurl = baseurl + "v1/cluster/me"
+    drtestconnect = drrbksess.get(url=testurl)
+    drtestresponse = drtestconnect.status_code
+    drtestjson = drtestconnect.json()
+    localprimaryclusterid = drtestjson['id']
+    clustermeresponse = drtestjson
+    print("Token Auth Test: "+str(drtestresponse))
+    return drrbksess, drbearer;
+
+def getSLAReplication(sla,objtype): 
+    global missingcount
+    basedrurl = "https://"+glob_rubrikip+"/api/"
+    if objtype == "Vms":
+        listurl = basedrurl+"v1/vmware/vm?effective_sla_domain_id="+sla
+        objtype = "VMWare VM"
+        snappath = "v1/vmware/vm/"
+        snapend = "/snapshot"
+        reversesort = False
+    if objtype == "Dbs":
+        listurl = basedrurl+"v1/mssql/db?effective_sla_domain_id="+sla
+        objtype = "MSSQL DB"
+        snappath = "v1/mssql/db/"
+        snapend = "/snapshot"
+        reversesort = False
+    if objtype == "NutanixVms":
+        listurl = basedrurl+"internal/nutanix/vm?effective_sla_domain_id="+sla
+        objtype = "Nutanix VM"
+        snappath = "internal/nutanix/vm/"
+        snapend = "/snapshot"
+        reversesort = True
+    if objtype == "HypervVms":
+        listurl = basedrurl+"internal/hyperv/vm?effective_sla_domain_id="+sla
+        objtype = "Hyper-V VM"
+        snappath = "internal/hyperv/vm/"
+        snapend = "/snapshot"
+        reversesort = False
+    if objtype == "Filesets":
+        listurl = basedrurl+"v1/fileset?effective_sla_domain_id="+sla
+        objtype = "Fileset"
+        snappath = "v1/fileset/"
+        filesetresponse = glob_rubrik_session.get(url=listurl)
+        filesetjson = filesetresponse.json()
+        for obj in filesetjson['data']:
+            glob_objlist['data'].append(obj)
+            glob_objlist['uuidlist'].append(obj['id'])
+        return()
+    #print(listurl)
+    listresponse = glob_rubrik_session.get(url=listurl)
+    #print(listresponse.text)
+    listjson = listresponse.json()
+    #print(json.dumps(listjson, indent=4))
+    for obj in listjson['data']:
+        glob_objlist['data'].append(obj)
+        glob_objlist['uuidlist'].append(obj['id'])
+    return()
+  
+
+#### Setup Rubrik
+glob_rubrikip = input("Enter Rubrik Hostname or IP: ")
+glob_user = input("Enter username: ")
+glob_pass = getpass.getpass('Enter Rubrik Password: ')
+glob_totp = input("Enter Totp: ")
+glob_objlist = {
+    "data": [],
+    "uuidlist": []
+    }
+
+localprimaryclusterid = ""
+clustermeresponse = {}
+
+glob_token = ""
+#glob_rubrik_session, glob_token = connectRubrik(glob_rubrikip, glob_user, glob_pass)
+glob_rubrik_session = rubrikSDK.getSession(glob_rubrikip, glob_user, glob_pass, glob_totp)
+
+outputfile = './'+glob_rubrikip+'replica_data.csv'
+
+glob_sourceCluster = input("Enter source cluster name: ")
+replication = rubrikSDK.getReplication("source", name=glob_sourceCluster)
+
+slaList = rubrikSDK.getSLADomain(primaryClusterId=replication['data'][0]['sourceClusterUuid'])
+
+
+for slajson in slaList['data']:
+    sla = slajson['id']
+    if slajson['numVms'] > 0:
+        objtype = "Vms"
+        getSLAReplication(sla,objtype)
+    if slajson['numDbs'] > 0:
+        objtype = "Dbs"
+        getSLAReplication(sla,objtype)
+    if slajson['numFilesets'] > 0:
+        objtype = "Filesets"
+        getSLAReplication(sla,objtype)
+    if slajson['numNutanixVms'] > 0:
+        objtype = "NutanixVms"
+        getSLAReplication(sla,objtype)
+    if slajson['numHypervVms'] > 0:
+        objtype = "HypervVms"
+        getSLAReplication(sla,objtype)
+
+#input("press any key to get response")
+#print(json.dumps(glob_objlist, indent=4))
+#input("halting here")
+
+url = "https://"+glob_rubrikip+"/api/v1/event/latest?limit=100&event_series_status=Success&event_status=Success&event_type=Replication&should_include_event_series=false"
+
+response = glob_rubrik_session.get(url=url)
+
+print("Beginning Event Pull")
+events = response.json()
+
+allevents = []
+keepGoing = True
+count = 1
+while keepGoing:
+    print("Pulling another 100...")
+    for event in events['data']:
+        if (event['latestEvent']['eventName'] == "Replication.ReplicationSucceeded") and (event['latestEvent']['objectId'] in glob_objlist['uuidlist']):
+            allevents.append(event)
+    print("Length of Events: "+str(len(events['data'])))
+    print("Current Count: "+str(count))
+    if (len(events['data']) == 100):
+        keepGoing = True
+        url = "https://"+glob_rubrikip+"/api/v1/event/latest?limit=100&event_series_status=Success&event_status=Success&event_type=Replication&after_id="+events['data'][99]['afterId']+"&should_include_event_series=false"
+        response = glob_rubrik_session.get(url=url)
+        try:
+            events = response.json()
+        except:
+            print("Failed response: "+str(response.text))
+        try:
+            print("Last Event: "+events['data'][99]['latestEvent']['id'])
+        except:
+            keepGoing = False
+        count = count + 1
+        if count > 20000:
+            keepGoing = False
+    else:
+        keepGoing = False
+
+print("Discovered "+str(len(allevents))+" Events of tiering. Building export...")
+total_count = len(allevents)
+count = 1
+for event in allevents:
+    url = "https://"+glob_rubrikip+"/api/v1/event_series/"+event['latestEvent']['eventSeriesId']
+    response = glob_rubrik_session.get(url=url)
+    data = response.json()
+    event['eventSummary'] = data
+    print(str(count)+"/"+str(total_count)+": pulling event for "+event['latestEvent']['objectName'])
+    count = count + 1
+
+print("Generating CSV File...")
+titlerow = "Object Name, Object Type, Event Type, SLA Name, Start Time, End Time, Data Transferrred (B)"
+with open(outputfile, 'w+') as csv:
+    csv.write(titlerow+"\n")
+    for event in allevents:
+        try:
+            if event['latestEvent']['eventName'] == "Replication.ReplicationSucceeded":
+                eventType = "Replication"
+            outputline = event['eventSummary']['objectName']+","+event['eventSummary']['objectType']+","
+            outputline = outputline+eventType+","+event['eventSummary']['slaName']
+            outputline = outputline+","+event['eventSummary']['startTime']+","+event['eventSummary']['endTime']+","
+            outputline = outputline+str(event['eventSummary']['dataTransferred'])+"\n"
+            csv.write(outputline)
+        except Exception as e:
+            print(e)
+            print(json.dumps(event, indent=4))
+            continue
+         
+        
+
---
+++ rubrikSDK.py
@@ -0,0 +1,279 @@
+# Assured Rubrik Development Tools
+# Created by Andrew Eva Jan 10, 2024
+# Purpose to simplify Rubrik Calls across automated scripts in Python
+
+# Modules
+import json
+import requests
+import urllib3
+import syslog
+import getpass
+from datetime import datetime
+from dateutil import parser
+
+# Silence warnings
+# requests.packages.urllib3.disable_warnings()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+globalsession = requests.session()
+globaltoken = ""
+globalhostname = ""
+globalcache = {}
+
+def sessionCreate(hostname, token):
+    global globalsession, globalhostname, globalcache
+    #Hostname expects a hostname, FQDN, or IP address without any prefix or suffix
+    #token expects the API Key or Token key of an authenticated Rubrik session
+    #Configure Requests Session
+    rubriksession = requests.Session()
+    rubriksession.verify = False
+    bearerToken = "Bearer "+token
+    header = { 'Authorization': bearerToken }
+    rubriksession.headers = header
+    target = "https://"+hostname+"/api/v1/cluster/me"
+    syslog.syslog(syslog.LOG_INFO, "Connecting to "+hostname+"...")
+    failed = False
+    try:
+        response = rubriksession.get(url=target)
+    except rubriksession.exceptions.Timeout:
+        syslog.syslog(syslog.LOG_ERR, hostname+" has timed out. Connection Failed")
+        failed = True
+    syslog.syslog(syslog.LOG_INFO, hostname+" status code: "+str(response.status_code))
+    if response.status_code == 200:
+        globalsession = rubriksession
+        globalhostname = hostname
+        globalcache['v1/cluster/me'] = response.json()
+        return rubriksession
+
+def connectCluster(hostname, **credentials):
+    global globalhostname
+    # Pass in the hostname, FQDN, or IP address without any prefix or suffic
+    # Also expecting "username" and "password" variables. If a TOTP variable is passed in then we will attempt to use it.
+    thesession = requests.Session()
+    thesession.verify = False
+    baseurl = "https://"+hostname+"/api/"
+    url = baseurl+"internal/session"
+    payload = {"initParams":{}}
+    if 'username' in credentials:
+        username = credentials['username']
+    else:
+        username = input("Username: ")
+    if 'password' in credentials:
+        password = credentials['password']
+    else:
+        password = getpass.getpass('Password: ')
+    if 'totp' in credentials:
+        code = credentials['totp']
+    else:
+        code = input("Enter TOTP Code: ")
+    response = thesession.post(url=url, auth=(username, password), json=payload)
+    attemptId = response.json()['mfaResponse']['attemptId']
+    data = {
+        "initParams":{}, 
+        "mfaParams":{ 
+            "attemptId":attemptId,
+            "challengeSetId":"N/A",
+            "challengeId":"rubrik-totp-auth",
+            "credValue":code
+            }
+        }
+    challengeResponse = thesession.post(url=url, timeout=120, json=data, auth=(username,password))
+    #print(challengeResponse.json())
+    orgId = challengeResponse.json()['session']['organizationId']
+    tagIt = credentials.get('keyTag',"rubrikADPSDK")
+    apiTokenData = {
+        "initParams": {
+            "apiToken": {
+                "expiration": 525000,
+                "tag": tagIt
+                },
+            "organizationId": orgId
+            }
+        }
+    try:
+        token = "Bearer "+challengeResponse.json()['session']['token']
+    except:
+        print("TOTP Authentication Failure, session not created")
+        return 
+    header = {'Authorization': token}
+    thesession.headers = header
+    saveToken = credentials.get('saveToken', False)
+    if credentials['saveToken']:
+        tokenCreateResponse = thesession.post(url=url, json=apiTokenData)
+        finalToken = tokenCreateResponse.json()['session']['token']
+    else:
+        finalToken = challengeResponse.json()['session']['token']
+    globalhostname = hostname
+    return finalToken
+
+def getSession(hostname, username, password, totp):
+    token = connectCluster(hostname, username=username, password=password, totp=totp, saveToken=False)
+    if token is None:
+        print("Unable to authenticate")
+        raise ExceptionGroup('Unable to Connect to Rubrik')
+    session = sessionCreate(hostname, token)
+    if session is None:
+        print("Unable to create session")
+        raise ExceptionGroup('Unable to create session')
+    return session
+
+def cache(key, value):
+    global globalcache
+    globalcache['key'] = value
+
+def getCache():
+    return globalcache
+
+def clearCache():
+    global globalcache
+    globalcache.clear()
+
+def getClusterId():
+    apiEndpoint = "v1/cluster/me"
+    if apiEndpoint in globalcache:
+        return globalcache[apiEndpoint]
+    else:
+        url = "https://"+globalhostname+"/api/v1/cluster/me"
+        response = globalsession.get(url=url)
+        data = response.json()
+        cache(apiEndpoint, data)
+        return data
+
+def getUser(userId):
+    apiEndpoint = "internal/user/"+userId
+    if apiEndpoint in globalcache:
+        return globalcache['apiEndpoint']
+    else:
+        url = "https://"+globalhostname+"/api/"+apiEndpoint
+        response = globalsession.get(url=url)
+        data = response.json()
+        cache(apiEndpoint, data)
+        return data
+
+def getUserSession(userId, **kwargs):
+    apiEndpoint = "internal/session?user_id="+userId
+    refreshCache = kwargs.get('refreshCache', False)
+    if (apiEndpoint in globalcache) and (not refreshCache):
+        return globalcache['apiEndpoint']
+    else:
+        url = "https://"+globalhostname+"/api/"+apiEndpoint
+        response = globalsession.get(url=url)
+        data = response.json()
+        cache(apiEndpoint, data)
+        return data
+
+def sortByDate(data, sortKey, **kwargs):
+    reverseOrder = kwargs.get('reverseOrder', False)
+    ascending = kwargs.get('ascending', True)
+    descending = kwargs.get('descending', False)
+    if descending:
+        reverseOrder = True
+    sorted_data = sorted(data, key=lambda x: parser.parse(x[sortKey]), reverse=reverseOrder)
+    return sorted_data
+
+def tokenCleanup(tokenKey, **kwargs):
+    apiEndpoint = "internal/session/bulk_delete"
+    userId = getUser(kwargs.get('userId',"me"))
+    tokenSummary = getUserSession(userId['id'][7:], refreshCache=True)
+    keepLatest = kwargs.get('keepLatest', False)
+    latestType = kwargs.get('latestType', 'lastUsageTime')
+    body = {
+        "tokenIds": [],
+        "userId": userId['id'][7:]
+        }
+    tokenSummary['data'] = sortByDate(tokenSummary['data'], latestType, reverseOrder=False)
+    for sess in tokenSummary['data']:
+        if sess['tag'] == tokenKey:
+            body['tokenIds'].append(sess['id'])
+    syslog.syslog(syslog.LOG_INFO, "Deleting "+str(len(body['tokenIds']))+" API Tokens that match "+tokenKey)
+    if keepLatest:
+        body['tokenIds'].pop()
+        syslog.syslog(syslog.LOG_INFO, "Most Recent Token Retained")
+    syslog.syslog(syslog.LOG_INFO, "Deleting "+str(len(body['tokenIds']))+" API Tokens that match "+tokenKey)
+    url = "https://"+globalhostname+"/api/"+apiEndpoint
+    response = globalsession.post(url=url, json=body)
+    if response.status_code == 204:
+        syslog.syslog(syslog.LOG_INFO, "Successful deletion of tokens matching "+tokenKey)
+    else:
+        syslog.syslog(syslog.LOG_ERR, "Deletion failed "+str(response.text))
+    
+def getNodes():
+    apiEndpoint = "internal/node"
+    if apiEndpoint in globalcache:
+        return globalcache[apiEndpoint]
+    else:
+        url = "https://"+globalhostname+"/api/"+apiEndpoint
+        response = globalsession.get(url=url)
+        data = response.json()
+        cache(apiEndpoint, data)
+        return data
+
+def getSLADomain(**kwargs):
+    apiEndpoint = "v2/sla_domain"
+    refreshCache = kwargs.get('refreshCache', False)
+    primaryClusterId = kwargs.get('primaryClusterId', "local")
+    apiEndpoint = apiEndpoint+"?primary_cluster_id="+primaryClusterId
+    if (apiEndpoint in globalcache) and (not refreshCache):
+        return globalcache[apiEndpoint]
+    else:
+        url = "https://"+globalhostname+"/api/"+apiEndpoint
+        response = globalsession.get(url=url)
+        data = response.json()
+        cache(apiEndpoint, data)
+        return data
+
+def getReplication(location, **kwargs):
+    apiEndpoint = "internal/replication"
+    if (location == "source") or (location == "target"):
+        apiEndpoint = apiEndpoint+"/"+location
+    else:
+        raise ExceptionGroup('Unknown location type, source or target are acceptable types.')
+    refreshCache = kwargs.get('refreshCache', False)
+    name = kwargs.get('name',"")
+    if (apiEndpoint in globalcache) and (not refreshCache):
+        return globalcache[apiEndpoint]
+    else:
+        url = "https://"+globalhostname+"/api/"+apiEndpoint
+        if len(kwargs) != 0:
+            count = 1
+            for key in kwargs.keys():
+                if count > 1:
+                    url = url+"&"
+                else:
+                    url = url+"?"
+                url = url+key+"="+kwargs[key]
+                count+=1
+        print(url)
+        response = globalsession.get(url=url)
+        globalcache[apiEndpoint] = response.json()
+        return response.json()
+
+def searchObject(searchdata):
+    # Requirements: needs to be sent the Rubrik search model JSON
+    #{"searchText":"string","searchProperties":["name"],"objectTypes":["AppBlueprint"],"primaryClusterId":"string","offset":0,"limit":0}
+    # No Caching of searches
+    apiEndpoint = "internal/hierarchy/search"
+    url = "https://"+globalhostname+"/api/"+apiEndpoint
+    body = searchdata
+    response = globalsession.post(url=url, json=body)
+    data = response.json()
+    return data
+
+def getObject(text, **kwargs):
+    # Default search Criteria
+    defaulttypes = [ "VirtualMachine", "WindowsHost", "HypervVirtualMachine","NutanixVirtualMachine","NasShare","NutanixVirtualMachine","MssqlDatabase","ManagedVolume","LinuxHost"]
+    body = {"searchText":"","searchProperties":["name"],"objectTypes": defaulttypes,"primaryClusterId":"","offset":0,"limit":100}
+    body['searchText'] = text
+    body['searchProperties'] = kwargs.get('searchProperties',["name"])
+    body['objectTypes'] = kwargs.get('objectTypes', defaulttypes)
+    body['primaryClusterId'] = kwargs.get('primaryClusterId', getClusterId()['id'])
+    if 'offset' in kwargs:
+        body['offset'] = kwargs['offset']
+    if 'limit' in kwargs:
+        body['limit'] = kwargs['limit']
+    results = searchObject(body)
+    print(json.dumps(results, indent=4))
+        
+    
+        
+    
+
