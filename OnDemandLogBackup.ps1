97e6c170908767e1572de4dc49a296cc987b3a6a
Andrew <andrew.eva@assured-dp.com>
2024-02-26 04:06:11 -0500

    Update OnDemandLogBackup.ps1
    Token options for this script

--- OnDemandLogBackup.ps1
+++ OnDemandLogBackup.ps1
@@ -4,12 +4,17 @@
 Import-Module Rubrik
 
 # Declare local variables. Can use credential file instead of "Get-Credential"
-$creds = Get-Credential
+# Token can be used instead of credentials if token is declared and token auth method used below
+# $creds = Get-Credential
 $dbname = "TestDataNew002"
 $rubrik = "10.51.23.240"
+$token = ""
 
-# Connect to Rubrik
-Connect-Rubrik -s $rubrik -Credential $creds
+# Connect to Rubrik with credentials
+# Connect-Rubrik -s $rubrik -Credential $creds
+
+# Connect to Rubrik with Token
+Connect-Rubrik -s $rubrik -token $token
 
 # Get the Database Unique ID
 $DB = Get-RubrikDatabase -Name $dbname
